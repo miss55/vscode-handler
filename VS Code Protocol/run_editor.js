c5835d8e6fe9281c690e52c541c4ed309b9e4e73
miss55 <810204005@qq.com>
2017-08-31 14:32:58 +0800

    Update run_editor.js
    In case your file is mapped via a network share and paths do not match.

--- VS Code Protocol/run_editor.js
+++ VS Code Protocol/run_editor.js
@@ -1,7 +1,11 @@
 var	url = WScript.Arguments(0),
     match = /^vscode:\/\/open\/\?url=file:\/\/(.+)&line=(\d+)$/.exec(url),
-    editor = 'code';
-
+    editor = 'code',
+    // In case your file is mapped via a network share and paths do not match.
+	// eg. /var/www will can replaced with Y:/
+	projects_basepath = '', 
+	projects_path_alias = '';
+	
 if (match) {
     // Add parameters
     editor += ' -g'; // Open the file at path at the line and column (add :line[:column] to path)
@@ -11,6 +15,11 @@ if (match) {
     // Parse file uri and replace to command
     // Reference from the docs: https://pla.nette.org/en/how-open-files-in-ide-from-debugger
     var file = decodeURIComponent(match[1]).replace(/\+/g, ' ');
+	
+	if (projects_basepath != '' && projects_path_alias != '') {
+		file = file.replace(new RegExp('^' + projects_basepath), projects_path_alias);
+	}
+	
     var command = editor.replace(/%line%/g, match[2]).replace(/%file%/g, file);
 
     // Run command in the shell
